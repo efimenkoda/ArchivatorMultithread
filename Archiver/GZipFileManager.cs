a99dbb849502dbe4d680d1219c73a15d8df61ee8
efimenkoda <dimas86@bk.ru>
2019-12-15 13:07:53 +0300

    Добавьте файлы проекта.

---
+++ Archiver/GZipFileManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archiver
+{
+    public class GZipFileManager
+    {
+        private string operation;
+        private string sourceFile;
+        private string destinationFile;
+        public GZipFileManager(string operation, string sourceFile, string destinationFile)
+        {
+            this.operation = operation;
+            this.sourceFile = sourceFile;
+            this.destinationFile = destinationFile;
+
+            CompresDecompress(sourceFile, destinationFile);
+        }
+
+        private void CompresDecompress(string sourceFile, string destinationFile)
+        {
+            using (FileStream sourceStream = new FileStream(sourceFile, FileMode.OpenOrCreate))
+            {
+                using (FileStream targetStream = File.Create(destinationFile))
+                {
+                    if (operation.Equals("compress"))
+                    {
+                            CreateGzip(sourceStream, targetStream);                           
+                        
+                    }
+                    else if (operation.Equals("decompress"))
+                    {                       
+                            ExtractFile(sourceStream, targetStream);                        
+                        
+                    }                    
+                }
+            }
+        }
+
+        private void CreateGzip(FileStream sourceStream, FileStream destinationStream)
+        {
+            using (GZipStream compressionStream = new GZipStream(destinationStream, CompressionMode.Compress))
+            {
+                sourceStream.CopyTo(compressionStream);
+                Console.WriteLine("Сжатие файла {0} завершено. Исходный размер: {1}  сжатый размер: {2}.",
+                            sourceFile, sourceStream.Length.ToString(), destinationStream.Length.ToString());
+            }
+
+        }
+
+        private void ExtractFile(FileStream sourceStream,FileStream destinationStream)
+        {
+            using (GZipStream decompressionStream = new GZipStream(sourceStream, CompressionMode.Decompress))
+            {
+                decompressionStream.CopyTo(destinationStream);
+                Console.WriteLine("Восстановлен файл: {0}", destinationFile);
+            }
+            
+        }
+
+
+    }
+}
---
+++ Archiver/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archiver
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            //string args0= "compress";
+            //string args1 = @"d:\TEMP\office.iso";
+            //string args2 = @"d:\TEMP\office.gz";
+
+            //args0 = "decompress";
+            //args1 = @"d:\TEMP\office.gz";
+            //args2 = @"d:\TEMP\officeNew.iso";
+
+            string args0 = @"compress";
+            string args1 = @"d:\TEMP\DatabaseVyrabotka.mdf";
+            string args2 = @"d:\TEMP\DatabaseVyrabotka.gz";
+            GZipFileManager gZipFile = new GZipFileManager(args0, args1, args2);
+
+            args0 = "decompress";
+            args1 = @"d:\TEMP\DatabaseVyrabotka.gz";
+            args2 = @"d:\TEMP\DatabaseVyrabotkaNew.mdf";
+            gZipFile = new GZipFileManager(args0, args1, args2);
+
+            Console.ReadLine();
+        }
+    }
+
+
+}
